bafa030b59d0a1241c3ada2d326eb3322c61a2e7
Pavel <mcredpower@gmail.com>
2018-03-13 10:59:48 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const body_parser = require('body-parser');
+const path = require('path');
+const mongojs = require('mongojs');
+const db = mongojs('CustomerControlApp', ['customers']);
+var ObjectId = mongojs.ObjectId;
+const app = express();
+
+app.use(body_parser.json());
+app.use(body_parser.urlencoded({
+  extended: false
+}));
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+app.post('/customer/add', function(req, res) {
+  var newCustomer = {
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    note: req.body.note
+  }
+  db.customers.insert(newCustomer, function(err, result) {
+    if (err) {
+      console.log(err);
+    }
+    console.log('inserted');
+      res.redirect('/');
+      res.render('index');
+  });
+});
+
+app.get('/admin', function(req, res) {
+  db.customers.find(function (err, docs) {
+  res.render('admin',{customers: docs});
+  });
+});
+
+app.delete('/customer/delete/:id',function(req,res){
+  db.customers.remove({_id: mongojs.ObjectId(req.params.id)}, function(err,result){
+    if (err) {
+console.log(err);
+    }
+    res.redirect('/admin')
+  })
+});
+
+
+app.listen(3000, function() {
+  console.log('server started on port 3000...');
+});
---
+++ public/script/deleterequest.js
@@ -0,0 +1,19 @@
+    $(document).ready(function() {
+      $( ".deletecustomer" ).on('click', deleteCustomer);
+    });
+
+    function deleteCustomer() {
+      var id = $(this).data("id");
+      var confirmation = confirm("Are you sure?");
+      if (confirmation) {
+        $.ajax({
+          type: 'DELETE',
+          url: '/customer/delete/'+ id
+        }).done(function(response) {
+          window.location.replace('/admin');
+        });
+        window.location.replace('/admin');
+      } else {
+        return false;
+      }
+    };
